7825c15af659a0088a4a8c655123fb357d622f52
alexissmc <133816957+alexissmc@users.noreply.github.com>
2024-11-27 19:35:25 -0600

    union de pasillos a sala

--- Scripts/sala.gd
+++ Scripts/sala.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # Usamos preload para cargar la escena
 var change_scene = load("res://Scenes/pasillo.tscn")
-var pasillo2_scene = load("res://Scenes/pasillo.tscn")
+var pasillo2_scene = load("res://Scenes/pasillo_2.tscn")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	# Cambiamos la escena usando la ruta del archivo directamente
