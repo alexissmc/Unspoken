fefc390f9c3944955b9bbc5d68d448226a303236
Diana Cruz <dianacd1234567@gmail.com>
2024-12-01 20:51:46 -0600

    niña

---
+++ Scripts/ninafantasma.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+var speed = 25
+var player_chase = false
+var player = null
+
+
+func _physics_process(delta: float) -> void:
+	if player_chase:
+		position += (player.position - position) / speed
+		
+		#$AnimatedSprite2D.play("left")
+		#if(player.position.x - position.x) < 0:
+			#$AnimatedSprite2D.flip_h = true
+		#else:
+			#$AnimatedSprite2D.flip_h = false
+			
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.play("right")
+		if (player.position.y - position.y) < 0:
+				$AnimatedSprite2D.play("down")
+		
+			
+	else:
+		$AnimatedSprite2D.play("down")
+
+
+
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
