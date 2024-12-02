67ed771f480672cb965efe7f4ae5ea66a090ae10
alexissmc <133816957+alexissmc@users.noreply.github.com>
2024-12-02 01:46:19 -0600

    cambios

--- Scripts/gameover.gd
+++ Scripts/gameover.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_play_pressed() -> void:
 	#get_tree().change_scene_to_file("res://Scenes/pasillo.tscn")
-	Transicion.change_scene("res://Scenes/pasillo.tscn", Vector2(313, 6))
+	Transicion.change_scene("res://Scenes/sala.tscn", Vector2(942, 350))
 	pass # Replace with function body.
 
 
