2a6c7f5963264e50807d2930cd2cfc5fe62f387b
Diana Cruz <dianacd1234567@gmail.com>
2024-12-02 00:53:37 -0600

    enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var speed = 70
 var player_chase = false
 var player = null
-var health = 100
+var health = 50
 var player_inattack_zone = false
 var can_take_damage = true
 func _physics_process(delta: float) -> void:
